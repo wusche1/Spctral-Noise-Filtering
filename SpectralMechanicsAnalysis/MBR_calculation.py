2502a8a18e9774da1af7b475bf3ab193d633d166
wusche1 <wuschelschulz8@gmail.com>
2024-02-28 19:23:10 +0100

    added unequal timesteps

--- SpectralMechanicsAnalysis/MBR_calculation.py
+++ SpectralMechanicsAnalysis/MBR_calculation.py
@@ -65,7 +65,112 @@ def mean_back_relaxation_multiple_trajectories(dt, trajectories, Tao, timespan, 
     else:
         return average_mbr
 
+
+def find_idx_of_nearest_value(array, value):
+    assert array.ndim == 1, "Array must be 1D"
+    i = 0
+    j = 0
+    nearest_idx_list = []
+    while i < len(array):
+        current_tao = array[i] - array[j]
+        next_tao = array[i] - array[j+1] if j+1 < len(array) else float('inf')  # Avoid IndexError
+        if i == j or abs(current_tao - value) <= abs(next_tao - value):  # Use <= to handle the case when they are equal
+            nearest_idx_list.append(j)
+            i += 1
+            if i < len(array):  # Check if i is within bounds after incrementing
+                current_tao = array[i] - array[j]
+                next_tao = array[i] - array[j+1] if j+1 < len(array) else float('inf')
+        else:
+            current_tao = next_tao
+            j += 1
+            next_tao = array[i] - array[j+1] if j+1 < len(array) else float('inf')
+    return nearest_idx_list
+def find_idx_of_nearest_value_right(array, value):
+    # Reverse the array and adjust the indices after calling the original function
+    reversed_array = array[::-1]
+    reversed_indices = find_idx_of_nearest_value(reversed_array, value)
+    # Calculate the original indices based on the length of the array
+    original_indices = [len(array) - 1 - idx for idx in reversed_indices]
+    return original_indices
+def mean_back_relaxation_with_timestamps(timestamps, trajectory, Tao, timespan, multiplier=1, d_sigma_estimation_samples = 1000, return_n_points_added=False, progress_bar = True, n_bins = 100, tao_tollerance_factor = 2):
+    if isinstance(trajectory, list):
+        trajectory = np.array(trajectory, dtype=np.float64)  # Ensure high precision
+    if isinstance(timestamps, list):
+        timestamps = np.array(timestamps, dtype=np.float64)
         
+    back_idx = find_idx_of_nearest_value(timestamps, Tao)
+    front_idx = find_idx_of_nearest_value_right(timestamps, timespan)
+
+    b_array = trajectory - trajectory[back_idx]
+    tao_array = timestamps - timestamps[back_idx]
+
+    d_sample = np.random.choice(b_array, size=min(d_sigma_estimation_samples, len(b_array)), replace=False)
+    d_sigma = np.std(d_sample)
+    d_cutoff = d_sigma * multiplier
+
+    mbr_bin_boundaries = np.linspace(0, timespan, n_bins)
+    mbr_time = (mbr_bin_boundaries[1:] + mbr_bin_boundaries[:-1]) / 2
+
+    valid_b = np.abs(b_array) > d_cutoff
+    #valid_tao = Tao/ tao_tollerance_factor < tao_array < Tao * tao_tollerance_factor
+    valid_tao = np.logical_and(Tao/ tao_tollerance_factor < tao_array, tao_array < Tao * tao_tollerance_factor)
+    # Identify indices where valid_b is True
+    valid_indices = np.where(valid_b & valid_tao)[0]
+
+    # Initialize arrays to store results
+    # Assuming max_bin_index is known or can be precomputed
+    mbr_trajectorie = np.zeros((len(mbr_bin_boundaries) - 1,))
+    mbr_n_data_points = np.zeros((len(mbr_bin_boundaries) - 1,))
+
+    # Loop over valid indices only
+    for i in tqdm(valid_indices, disable=not progress_bar, total=len(valid_indices)):
+        range_forward = front_idx[i]
+        sample = trajectory[i:range_forward] - trajectory[i]
+        t_sample = timestamps[i:range_forward] - timestamps[i]
+
+        b_mbr = b_array[i]
+        sample_to_add = (-sample / b_mbr).astype(np.float64)
+
+        # Bin mapping for the entire sample
+        bin_mapping = np.digitize(t_sample, mbr_bin_boundaries) - 1
+
+        # Avoid adding to the last bin if not applicable
+        valid_bins = bin_mapping != len(mbr_bin_boundaries) - 1
+
+        # Use numpy to sum and count efficiently
+        np.add.at(mbr_trajectorie, bin_mapping[valid_bins], sample_to_add[valid_bins])
+        np.add.at(mbr_n_data_points, bin_mapping[valid_bins], 1)
+
+    # Normalize the accumulated samples, ensuring no division by zero
+    mbr_trajectorie = np.divide(mbr_trajectorie, mbr_n_data_points, where=mbr_n_data_points != 0)
+
+    if return_n_points_added:
+        return mbr_time, mbr_trajectorie, mbr_n_data_points
+    return mbr_time, mbr_trajectorie
+
+def mean_back_relaxation_multiple_trajectories_with_timestamps(timestamps_list, trajectory_list, Tao, timespan, multiplier=1, d_sigma_estimation_samples = 1000, return_n_points_added=False, progress_bar = True, n_bins = 100, tao_tollerance_factor = 2):
+
+    mbr_list = []
+    mbr_n_data_points_list = []
+
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(mean_back_relaxation_with_timestamps, timestamps, trajectory, Tao, timespan, multiplier, d_sigma_estimation_samples, True, False, n_bins, tao_tollerance_factor) for timestamps, trajectory in zip(timestamps_list, trajectory_list)]
+        for future in tqdm(futures, total=len(futures)):
+            mbr_time, mbr, mbr_n_data_points = future.result()
+            mbr_list.append(mbr)
+            mbr_n_data_points_list.append(mbr_n_data_points)
+    
+    average_mbr = []
+    for i in range(len(mbr_time)):
+        mbr_weights = [mbr_n_data_points[i] for mbr_n_data_points in mbr_n_data_points_list]
+        mbr_values = [mbr[i] for mbr in mbr_list]
+        mbr_weights = np.array(mbr_weights)
+        mbr_values = np.array(mbr_values)
+        mbr_weighted = sum(mbr_weights*mbr_values)/sum(mbr_weights)
+        average_mbr.append(mbr_weighted)
+    
+
+    return mbr_time, average_mbr
 
 
     
