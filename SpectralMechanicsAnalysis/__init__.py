3f37f0cd586ec35a72978dc4e948657458efd435
wusche1 <wuschelschulz8@gmail.com>
2024-02-28 19:25:37 +0100

    added new funciton to setup

--- SpectralMechanicsAnalysis/__init__.py
+++ SpectralMechanicsAnalysis/__init__.py
@@ -1,4 +1,4 @@
 from .data_handeling import Data 
-from .MBR_calculation import mean_back_realxation, mean_back_relaxation_multiple_trajectories
+from .MBR_calculation import mean_back_realxation, mean_back_relaxation_multiple_trajectories, mean_back_relaxation_with_timestamps, mean_back_relaxation_multiple_trajectories_with_timestamps
 
-__all__ = ['Data', 'mean_back_realxation', 'mean_back_relaxation_multiple_trajectories']
+__all__ = ['Data', 'mean_back_realxation', 'mean_back_relaxation_multiple_trajectories', 'mean_back_relaxation_with_timestamps', 'mean_back_relaxation_multiple_trajectories_with_timestamps']
